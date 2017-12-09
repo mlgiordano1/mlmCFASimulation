9ff445c964d95a5bab86f283e33b5d1e590d4dfb
Michael Giordano <mlgiordano1@gmail.com>
2017-12-09 15:35:38 -0500

    Continuing the goldstein estimator...still not done

--- temp/fittinggoldstein.R
+++ temp/fittinggoldstein.R
@@ -1,7 +1,10 @@
+# Decisions to be made
+# Nlme vs LME?
+# fill in matrices, two at a time, or three at a time?
+# remove irrelevant rows when not in use?
+# play around with different optimizers
 
-# REAd in the data
-# Make the data wide and dummy code the indicators
-# fit the models
+
 
 library('nlme')
 library('lme4')
@@ -9,22 +12,33 @@ library("reshape2")
 library('dummies')
 library('dplyr')
 
-
+# ------------------------------------------------------------------------------
+# Process Data
 myData <- read.table(file = "C:/users/mgiordan/git/mlmcfasimulation/temp/tempdata_1.dat")
 names(myData) <- c(paste0("y", 1:9), "cluster")
 myData$id <- 1:nrow(myData)
-
 # make data long
 long <- melt(myData, id.vars = c("id", "cluster"), variable.name = "item")
 # dummy code the indicators
 for (level in unique(long$item)) {
   long[paste("dummy", level, sep = "_")] <- ifelse(long$item == level, 1, 0)
 }
+# ------------------------------------------------------------------------------
 
-allComb <- t(combn(c("y1", "y2", "y3", "y4", "y5", "y6", "y7", "y8", "y9"), m=2))
+namesOfIndicators <- c("y1", "y2", "y3", "y4", "y5", "y6", "y7", "y8", "y9")
+combinations <- t(combn(namesOfIndicators, m=2))
+
+
+# fitting with different numbers of indicators 
+fit <- lme (value~-1+dummy_y1+dummy_y2+dummy_y3, random =~ -1+dummy_y1+dummy_y2+dummy_y3|cluster/id, data=long )
+summary(fit)
+fit <- lmer(value~-1+dummy_y1+dummy_y2+dummy_y3+ (-1+dummy_y1+dummy_y2+dummy_y3|cluster/id), data=long )
+summary(fit)
+VarCorr(fit)
 
 allResults <- list()
-mylist <- list()
+
+
 
 startTime <- proc.time()
 # go through all the combinations
@@ -78,13 +92,43 @@ for (i in seq(nrow(allComb))) {
   tryCatch({
     print(i)
     fit <- lmer(value~-1+var1+var2+ (-1+var1+var2|cluster/id), data=temp )
-    mylist[[paste0(allComb[i,], collapse="")]] = VarCorr(fit)
+    allResults[[paste0(allComb[i,], collapse="")]] <- VarCorr(fit)
   }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
 }
 endTime <- proc.time()
 
 paste("The loop took (in seconds):")
 endTime-startTime
+
+
+
+# Step 2 - fill within covariance matrix
+allResults["y1y2"]
+
+#make a matrix
+within <- matrix(nrow=9, ncol=9, 
+                 dimnames = list(c("y1", "y2", "y3", "y4", "y5", "y6", "y7", "y8", "y9"),
+                                 c("y1", "y2", "y3", "y4", "y5", "y6", "y7", "y8", "y9")))
+diag(within) <- 1
+# go through results and fill in 
+for (i in seq(nrow(allComb))) {
+  #save the vars being used as a vector
+  vars <- allComb[i,]
+  # save the as a combined name as well
+  name <- paste(allComb[i,], collapse = "")
+  # pulling out the relevant number
+  # dim(allResults[[name]])
+  # str(allResults[[name]])
+  # allResults[[name]]$`id:cluster`[1,2]
+  # fill in the lower diag
+  within[vars[2], vars[1]] <- allResults[[name]]$`id:cluster`[1,2]
+  # fill in the upper diag
+  within[vars[1], vars[2]] <- allResults[[name]]$`id:cluster`[1,2]
+}
+
+length(allResults)
+
+
 
 mylist["y1y2"]
 
