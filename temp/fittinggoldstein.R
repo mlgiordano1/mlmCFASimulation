a604587665c4904d7ac62b11e6e4c658eb045b10
Michael Giordano <mlgiordano1@gmail.com>
2017-12-20 09:26:31 -0500

    testing the full goldstein function

--- temp/fittinggoldstein.R
+++ temp/fittinggoldstein.R
@@ -35,33 +35,62 @@ source("goldsteinFunction.R")
 myData <- read.table(file = "C:/users/mgiordan/git/mlmcfasimulation/temp/tempdata_1.dat")
 names(myData) <- c(paste0("y", 1:9), "cluster")
 myData$id <- 1:nrow(myData)
-# make data long
-long <- reshape2::melt(myData, id.vars = c("id", "cluster"), variable.name = "item")
-# dummy code the indicators
-for (level in unique(long$item)) {
-  long[paste("dummy", level, sep = "_")] <- ifelse(long$item == level, 1, 0)
-  long[level] <- ifelse(long$item == level, 1, 0)
-}
+# # make data long
+# long <- reshape2::melt(myData, id.vars = c("id", "cluster"), variable.name = "item")
+# # dummy code the indicators
+# for (level in unique(long$item)) {
+#   long[paste("dummy", level, sep = "_")] <- ifelse(long$item == level, 1, 0)
+#   long[level] <- ifelse(long$item == level, 1, 0)
+# }
 # ------------------------------------------------------------------------------
 
 # save some values
 indicators = c("y1", "y2", "y3", "y4", "y5", "y6", "y7", "y8", "y9")
 
-temp <- goldstein(longDF = long,
-                  outcome = "value",
-                  allIndicators = indicators)
+bModel <- '
+l1 =~ y1+y2+y3+y4+y5+y6+y7+y8+y9
+'
+
+wModel <- '
+l1=~y1+y2+y3+y5
+l2=~y4+y5+y6+y8
+l3=~y7+y8+y9+y6
+l1~~l2
+l1~~l3
+l2~~l3'
+
 
 # TESting the time
 startTime <- proc.time()
-temp <- goldstein(longDF = long,
-                  outcome = "value",
-                  fitWith = 'nlme', 
-                  allIndicators = indicators)
+temp <- goldstein(withinModel = wModel,
+                  betweenModel = bModel, 
+                  allIndicators = indicators,
+                  l1Var = "id", 
+                  l2Var = "cluster", 
+                  df = myData) 
 endTime <- proc.time()
 endTime-startTime
 View(temp$within)
 
+temp$within
+temp$between
 
+withinModel = wModel
+betweenModel = bModel 
+allIndicators = indicators
+l1Var = "id"
+l2Var = "cluster"
+df = myData
+
+temp$within
+
+
+
+MIIVsem::miive(wModel, sample.cov = temp$within, sample.nobs = 1000)
+
+MIIVsem::miive(bModel, sample.cov = temp$between, sample.nobs = 100)
+
+?MIIVsem::miive
 
 
 
