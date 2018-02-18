4fe3c9765906432da90f670787581b1f79008856
Michael Giordano <mlgiordano1@gmail.com>
2018-02-17 22:06:50 -0500

    setting up mplus fitting for the multiple misSpecifications

--- presentationSim/fitMplus.R
+++ presentationSim/fitMplus.R
@@ -16,28 +16,38 @@ mpluswModelMis <- '
 l1 by y1-y3;
 l2 by y4-y6;
 '
+mpluswModelMis1 <- '
+l1 by y1-y3
+l2 by y4-y6 y2;
+y2 with y3;
+l1 with l2;
+'
+mpluswModelMis2 <- '
+l1 by y1-y3 y5;
+l2 by y4-y6
+y2 with y3;
+l1 with l2;
+'
+mpluswModelMis3 <- '
+l1 by y1-y3 y5;
+l2 by y4-y6 y2;
+! missing correlated error
+l1 with l2;
+'
 
+# reading in the parameters of the model
 simParams     <- readRDS("SimParams.rds")
 designMatrix  <- simParams$designMatrix
 iterationsPer <- simParams$iterationsPer
 wModelTrue    <- simParams$wModelTrue
 wModelMis     <- simParams$wModelMis
+wModelMis1    <- simParams$wModelMis1
+wModelMis2    <- simParams$wModelMis2
+wModelMis3    <- simParams$wModelMis3
 bModelTrue    <- simParams$bModelTrue
-
-designMatrix$inpName <- paste0("savedModels", "/",
-                              designMatrix$clusterBal, "_",
-                              designMatrix$clusterSize, "_",
-                              designMatrix$clusterN, "_",
-                              designMatrix$modelSpec, "_",
-                              designMatrix$distribution, "_",
-                              designMatrix$estimators,
-                              designMatrix$Iteration,
-                              ".inp")
-
 
 # subset just the estimator we want
 designMatrix <- designMatrix[which(designMatrix$estimators=="FIML"),]
-
 
 for (i in seq(nrow(designMatrix))) { # startingPoint!
   # Save the model we will be using
@@ -46,6 +56,15 @@ for (i in seq(nrow(designMatrix))) { # startingPoint!
     bModel <- mplusBModel
   }  else if (designMatrix$modelSpec[i] == "misSpec") {
     wModel <- mpluswModelMis
+    bModel <- mplusBModel
+  } else if (designMatrix$modelSpec[i] == "misSpec1") {
+    wModel <- mpluswModelMis1
+    bModel <- mplusBModel
+  } else if (designMatrix$modelSpec[i] == "misSpec2") {
+    wModel <- mpluswModelMis2
+    bModel <- mplusBModel
+  } else if (designMatrix$modelSpec[i] == "misSpec3") {
+    wModel <- mpluswModelMis3
     bModel <- mplusBModel
   }
   
@@ -62,4 +81,4 @@ for (i in seq(nrow(designMatrix))) { # startingPoint!
               con = designMatrix$inpName[i])
 }
 
-MplusAutomation::runModels("./savedModels" )
+# MplusAutomation::runModels("./savedModels" )
