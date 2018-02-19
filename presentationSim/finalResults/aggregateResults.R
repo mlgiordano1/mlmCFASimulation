396d22a837764d379de9341d20b41c58cde68cb0
Michael Giordano <mlgiordano1@gmail.com>
2018-02-18 20:07:01 -0500

    cleaning up the aggregation script

--- presentationSim/finalResults/aggregateResults.R
+++ presentationSim/finalResults/aggregateResults.R
@@ -1,18 +1,14 @@
 library(tidyr)
 library(dplyr)
 library(ggplot2)
+library(stringr)
 
 baseDir <- "c:/users/mgiordan/git/mlmcfasimulation/presentationsim"
 setwd(baseDir)
 
 # allMplusModels <- MplusAutomation::readModels("savedModels")
 # saveRDS(allMplusModels, "savedModels/allMplusModels.rds")
-
 m <- readRDS("savedModels/allMplusModels.rds")
-
-# m[1]
-# m$bal_100_100_misspec_normal_fiml1.out$parameters
-
 
 # read in the design matrix
 dm <- readRDS("SimParams.rds")
@@ -21,8 +17,15 @@ dm <- dm$designMatrix
 # go one at a time through the design matrix
 for (i in seq(nrow(dm))) {
   print(i)
+  try({
   if (dm$estimators[i]=="FIML") {
-  mName <- tolower(paste0(substr(dm$rdsName[i], 13, nchar(dm$rdsName[i])-3), "out"))
+  mName <- paste0(tolower(paste(dm$clusterBal[i], 
+                   dm$clusterSize[i], 
+                    dm$clusterN[i], 
+                    dm$modelSpec[i], 
+                    dm$distribution[i], 
+                    dm$estimators[i], 
+                    dm$Iteration[i], sep = "_")), ".out")
   mod <- m[[mName]]
   param <- mod[["parameters"]][["unstandardized"]]
   param$name <- paste0(param$paramHeader, param$param)
@@ -32,6 +35,13 @@ for (i in seq(nrow(dm))) {
   dm[i, "l2.by.y4"] <- as.numeric(param[param$name=="L2.BYY4", "est"])
   dm[i, "l2.by.y5"] <- as.numeric(param[param$name=="L2.BYY5", "est"])
   dm[i, "l2.by.y6"] <- as.numeric(param[param$name=="L2.BYY6", "est"])
+  
+  #dm[i, "LB.by.y1"] <- as.numeric(param[param$name=="L4.BYY1", "est"])
+  dm[i, "LB.by.y2"] <- as.numeric(param[param$name=="L4.BYY2", "est"])
+  dm[i, "LB.by.y3"] <- as.numeric(param[param$name=="L4.BYY3", "est"])
+  dm[i, "LB.by.y4"] <- as.numeric(param[param$name=="L4.BYY4", "est"])
+  dm[i, "LB.by.y5"] <- as.numeric(param[param$name=="L4.BYY5", "est"])
+  dm[i, "LB.by.y6"] <- as.numeric(param[param$name=="L4.BYY6", "est"])
   # add in the misspecified parameters
   } else {
   mod <- readRDS(dm$rdsName[i])
@@ -41,11 +51,20 @@ for (i in seq(nrow(dm))) {
   # dm[i, "l2.by.y4"] <- mod$within$coefficients[["y4~l2"]]
   dm[i, "l2.by.y5"] <- mod$within$coefficients[["y5~l2"]]
   dm[i, "l2.by.y6"] <- mod$within$coefficients[["y6~l2"]]
+  
+  dm[i, "LB.by.y2"] <- mod$between$coefficients[["y2~l1"]]
+  dm[i, "LB.by.y3"] <- mod$between$coefficients[["y3~l1"]]
+  dm[i, "LB.by.y4"] <- mod$between$coefficients[["y4~l1"]]
+  dm[i, "LB.by.y5"] <- mod$between$coefficients[["y5~l1"]]
+  dm[i, "LB.by.y6"] <- mod$between$coefficients[["y6~l1"]]
+
   } # end the if
+  })# end of try
 } # end for loop
 
+saveRDS(dm, "savedModels/results.rds")
 
-dmLong <- gather(dm, paramter, est, l1.by.y2:l2.by.y4, factor_key = TRUE) 
+dmLong <- gather(dm, paramter, est, l1.by.y2:LB.by.y6, factor_key = TRUE) 
 
 # put in the true values
 dmLong[dmLong$paramter=="l1.by.y1", "true"] <- 1
@@ -54,6 +73,12 @@ dmLong[dmLong$paramter=="l1.by.y3", "true"] <- .7
 dmLong[dmLong$paramter=="l2.by.y4", "true"] <- 1
 dmLong[dmLong$paramter=="l2.by.y5", "true"] <- .8
 dmLong[dmLong$paramter=="l2.by.y6", "true"] <- .7
+# tru values for between parameters
+dmLong[dmLong$paramter=="LB.by.y2", "true"] <- .7
+dmLong[dmLong$paramter=="LB.by.y3", "true"] <- .6
+dmLong[dmLong$paramter=="LB.by.y4", "true"] <- .8
+dmLong[dmLong$paramter=="LB.by.y5", "true"] <- .7
+dmLong[dmLong$paramter=="LB.by.y6", "true"] <- .8
 
 # Remove the scaling indicators
 dmLong <- dmLong[which(dmLong$paramter!="l1.by.y1"),]
@@ -69,9 +94,35 @@ saved <- dmLong %>%
             true  = mean(true),
             rel.bias = ((est-true)/true))
 
-ggplot(dmLong, aes(x=paramter, y = relBias, fill = estimators)) +
+ggplot(dmLong[which(dmLong$modelSpec=="trueModel"),], aes(x=paramter, y = relBias, fill = estimators)) +
   geom_boxplot() +
-  facet_grid(clusterSize~modelSpec) +
+  facet_grid(modelSpec~clusterSize) +
+  geom_hline(yintercept = .2) + 
+  geom_hline(yintercept = -.2) + 
+  geom_hline(yintercept = 0, color = "red", linetype = "dashed") +
   scale_y_continuous(limits = c(-.5,1))
+
+ggplot(dmLong[which(dmLong$modelSpec=="misSpec1"),], aes(x=paramter, y = relBias, fill = estimators)) +
+  geom_boxplot() +
+  facet_grid(modelSpec~clusterSize) +
+  geom_hline(yintercept = .2) + 
+  geom_hline(yintercept = -.2) + 
+  scale_y_continuous(limits = c(-.5,1))
+
+ggplot(dmLong[which(dmLong$modelSpec=="misSpec2"),], aes(x=paramter, y = relBias, fill = estimators)) +
+  geom_boxplot() +
+  facet_grid(modelSpec~clusterSize) +
+  geom_hline(yintercept = .2) + 
+  geom_hline(yintercept = -.2) + 
+  geom_hline(yintercept = 0, color = "red", linetype = "dashed") +
+  scale_y_continuous(limits = c(-.5,1))
+
+ggplot(dmLong[which(dmLong$modelSpec=="misSpec3"),], aes(x=paramter, y = relBias, fill = estimators)) +
+  geom_boxplot() +
+  facet_grid(modelSpec~clusterSize) +
+  geom_hline(yintercept = .2) + 
+  geom_hline(yintercept = -.2) + 
+  scale_y_continuous(limits = c(-.5,1))
+
 
 
