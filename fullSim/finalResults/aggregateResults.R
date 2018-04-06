c7c92ed82a809c42b86bbcd746d9778b0706a954
Michael Giordano <mlgiordano1@gmail.com>
2018-04-06 15:20:08 -0400

    Processing subsets for comparisons here to save comp time in .rmd script

--- fullSim/finalResults/aggregateResults.R
+++ fullSim/finalResults/aggregateResults.R
@@ -218,198 +218,131 @@ source("../SimulationFunctions.R")
 #                          by = "rdsName")
 # # save all!
 # saveRDS(allResults, "finalResults/allResultsDf.rds")
+# make all long
+
+
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+# Now we are going to process subsets for the results section
+# originally I was doing the processing in the .RMD file but that caused problems
+# Now we make the subsets, and basically use the .RMD file to make the tables/figures
+# --------------------------------------------------------------------------
+# read in allresults
 dm <- readRDS("finalResults/allResultsDf.rds")
-
-# # make a single skew condition var
-# dm$skewKurt <- paste0(dm$wSkew, dm$wKurt, dm$bSkew, dm$bKurt)
-n <-  c("Iteration", "clusterSize", "clusterN", "clusterBal", 
-  "modelSpec", "estimators", "skewKurt", "dfName", "datName", 
-  "sampleSize", "rdsName", "inpName")
-loadings <- c("l1.by.y1",
-"l1.by.y2",
-"l1.by.y3",
-"l1.by.y5",
-"l2.by.y4",
-"l2.by.y5",
-"l2.by.y6",
-"l2.by.y2",
-"l1.by.y3",
-"lb.by.y1",
-"lb.by.y2",
-"lb.by.y3",
-"lb.by.y4",
-"lb.by.y5",
-"lb.by.y6")
-
-# loadings long
-loadLong <- gather(dm[c(n, loadings)], 
-  paramter, est, l1.by.y1:lb.by.y6, factor_key = TRUE) 
-# convert to numeric
-loadLong$est <- as.numeric(loadLong$est)
-
-# put in the true values
-loadLong[loadLong$paramter=="l1.by.y1", "true"] <- 1
-loadLong[loadLong$paramter=="l1.by.y2", "true"] <- .8
-loadLong[loadLong$paramter=="l1.by.y3", "true"] <- .7
-loadLong[loadLong$paramter=="l2.by.y4", "true"] <- 1
-loadLong[loadLong$paramter=="l2.by.y5", "true"] <- .8
-loadLong[loadLong$paramter=="l2.by.y6", "true"] <- .7
-# tru values for between parameters
-loadLong[loadLong$paramter=="LB.by.y2", "true"] <- .7
-loadLong[loadLong$paramter=="LB.by.y3", "true"] <- .6
-loadLong[loadLong$paramter=="LB.by.y4", "true"] <- .8
-loadLong[loadLong$paramter=="LB.by.y5", "true"] <- .7
-loadLong[loadLong$paramter=="LB.by.y6", "true"] <- .8
-# Remove the scaling indicators
-loadLong <- loadLong[which(loadLong$paramter!="l1.by.y1"),]
-loadLong <- loadLong[which(loadLong$paramter!="l2.by.y4"),]
-
-#relative bias
-loadLong$relBias <- (loadLong$est - loadLong$true) / loadLong$true
-names(loadLong)
-btwFactors <- c("clusterSize", "clusterN", "clusterBal", "skewKurt")
-wFactors <- c("modelSpec", "Estimators")
-
-dmLong <- dmLong[complete.cases(dmLong),]
-
-saved <- loadLong %>%
-  group_by(clusterSize, clusterN, clusterBal, skewKurt, modelSpec, estimators, paramter) %>%
-  summarize(count = n(),
-            mean  = mean(est, na.rm = TRUE),
-            meanBias = mean(relBias, na.rm = TRUE),
-            meanBias2 = sum((est-true)/true, na.rm = TRUE)/n(),
-            rmse = sqrt(sum((est-true)^2, na.rm = TRUE)/n()),
-            true  = mean(true))
-# need to do some reshaping
-temp <- reshape2::dcast(saved, clusterSize + clusterN + clusterBal + skewKurt ~ modelSpec+paramter+estimators, value.var = "mean" )
-# save as a csv so we can edit
-write.csv(x = temp, file = "finalResults/parametersTable.csv")
-
-
-saved$btwKey <- paste0(saved$clusterSize, saved$clusterN, saved$clusterBal, saved$skewKurt)
-
-
-temp <- reshape(saved, v.names = "mean", timevar = c("modelSpec", "estimators", "paramter"), idvar = "btwKey", direction = "wide")
-
-?reshape()
-
-
-reshape(dat1, idvar = "name", timevar = "numbers", direction = "wide")
-# pulling just the within parameters
-within <- c("l1.by.y2", "l1.by.y3", "l2.by.y5", "l2.by.y6")
-within <- dmLong[dmLong$paramter %in% within,]
-# want to drop some misSpec Conditions
-within <- within[!(within$modelSpec %in% "misSpec"),]
-within <- within[!(within$modelSpec %in% "misSpec1"),]
-# do some renaming
-within[which(within$modelSpec=="misSpec1"), "modelSpec"] <- "Missing L1 to Y5"
-within[which(within$modelSpec=="misSpec2"), "modelSpec"] <- "Missing L2 to Y2"
-within[which(within$modelSpec=="misSpec3"), "modelSpec"] <- "msg corr err"
-# re-ordering factors
-within$modelSpec <- factor(within$modelSpec, levels(factor(within$modelSpec))[c(3,1:2)])
-
-# pulling just the within parameters
-between <- c("LB.by.y2", "LB.by.y3","LB.by.y4", "LB.by.y5", "LB.by.y6")
-between <- dmLong[dmLong$paramter %in% between,]
-# want to drop some misSpec Conditions
-between <- between[!(between$modelSpec %in% "misSpec"),]
-between <- between[!(between$modelSpec %in% "misSpec1"),]
-# do some renaming
-between[which(between$modelSpec=="misSpec1"), "modelSpec"] <- "Missing L1 to Y5"
-between[which(between$modelSpec=="misSpec2"), "modelSpec"] <- "Missing L2 to Y2"
-between[which(between$modelSpec=="misSpec3"), "modelSpec"] <- "msg corr err"
-# re-ordering factors
-between$modelSpec <- factor(between$modelSpec, levels(factor(between$modelSpec))[c(3,1:2)])
-
-ggplot(within[within$modelSpec=="trueModel",], aes(x=paramter, y = relBias, fill = estimators)) +
-  geom_boxplot() +
-  facet_grid(~skewKurt) +
-  geom_hline(yintercept = .2) + 
-  geom_hline(yintercept = -.2) + 
-  geom_hline(yintercept = 0, color = "red", linetype = "dashed") +
-  scale_y_continuous(limits = c(-.5,.5), breaks = seq(from = -.8, to = .8, by = .2)) +
-  theme(axis.text.x=element_text(angle=45,hjust=1,vjust=0.5)) +
-  ggtitle("Within: All 3 specifications")
-ggsave("finalResults/Within All 3 specifications.jpeg")
-
-ggplot(within[which(within$modelSpec=="trueModel"),], aes(x=paramter, y = relBias, fill = estimators)) +
-  geom_boxplot() +
-  #facet_grid(~modelSpec) +
-  geom_hline(yintercept = .2) + 
-  geom_hline(yintercept = -.2) + 
-  geom_hline(yintercept = 0, color = "red", linetype = "dashed") +
-  scale_y_continuous(limits = c(-.5,.5), breaks = seq(from = -.8, to = .8, by = .2)) +
-  theme(axis.text.x=element_text(angle=45,hjust=1,vjust=0.5)) +
-  ggtitle("Within: True Model")
-ggsave("finalResults/Within True Model.jpeg")
-
-ggplot(within[which(within$modelSpec=="Missing L2 to Y2"),], aes(x=paramter, y = relBias, fill = estimators)) +
-  geom_boxplot() +
-  #facet_grid(~modelSpec) +
-  geom_hline(yintercept = .2) + 
-  geom_hline(yintercept = -.2) + 
-  geom_hline(yintercept = 0, color = "red", linetype = "dashed") +
-  scale_y_continuous(limits = c(-.5,.5), breaks = seq(from = -.8, to = .8, by = .2)) +
-  theme(axis.text.x=element_text(angle=45,hjust=1,vjust=0.5)) +
-  ggtitle("Within: Missing Cross Loading")
-ggsave("finalResults/Within Missing Cross Loading.jpeg")
-
-ggplot(within[which(within$modelSpec=="msg corr err"),], aes(x=paramter, y = relBias, fill = estimators)) +
-  geom_boxplot() +
-  #facet_grid(~modelSpec) +
-  geom_hline(yintercept = .2) + 
-  geom_hline(yintercept = -.2) + 
-  geom_hline(yintercept = 0, color = "red", linetype = "dashed") +
-  scale_y_continuous(limits = c(-.5,.5), breaks = seq(from = -.8, to = .8, by = .2)) +
-  theme(axis.text.x=element_text(angle=45,hjust=1,vjust=0.5)) +
-  ggtitle("Within: Missing Correlated Error")
-ggsave("finalResults/Within Missing Correlated Error.jpeg")
-
-
-ggplot(between, aes(x=paramter, y = relBias, fill = estimators)) +
-  geom_boxplot() +
-  facet_grid(~skewKurt) +
-  geom_hline(yintercept = .2) + 
-  geom_hline(yintercept = -.2) + 
-  geom_hline(yintercept = 0, color = "red", linetype = "dashed") +
-  scale_y_continuous(limits = c(-.5,.5), breaks = seq(from = -.8, to = .8, by = .2)) +
-  theme(axis.text.x=element_text(angle=45,hjust=1,vjust=0.5)) +
-  ggtitle("between: All 3 specifications")
-ggsave("finalResults/between All 3 specifications.jpeg")
-
-ggplot(between[which(between$modelSpec=="trueModel"),], aes(x=paramter, y = relBias, fill = estimators)) +
-  geom_boxplot() +
-  #facet_grid(~modelSpec) +
-  geom_hline(yintercept = .2) + 
-  geom_hline(yintercept = -.2) + 
-  geom_hline(yintercept = 0, color = "red", linetype = "dashed") +
-  scale_y_continuous(limits = c(-.5,.5), breaks = seq(from = -.8, to = .8, by = .2)) +
-  theme(axis.text.x=element_text(angle=45,hjust=1,vjust=0.5)) +
-  ggtitle("between: True Model")
-ggsave("finalResults/between True Model.jpeg")
-
-ggplot(between[which(between$modelSpec=="Missing L2 to Y2"),], aes(x=paramter, y = relBias, fill = estimators)) +
-  geom_boxplot() +
-  #facet_grid(~modelSpec) +
-  geom_hline(yintercept = .2) + 
-  geom_hline(yintercept = -.2) + 
-  geom_hline(yintercept = 0, color = "red", linetype = "dashed") +
-  scale_y_continuous(limits = c(-.5,.5), breaks = seq(from = -.8, to = .8, by = .2)) +
-  theme(axis.text.x=element_text(angle=45,hjust=1,vjust=0.5)) +
-  ggtitle("between: Missing Cross Loading")
-ggsave("finalResults/between Missing Cross Loading.jpeg")
-
-ggplot(between[which(between$modelSpec=="msg corr err"),], aes(x=paramter, y = relBias, fill = estimators)) +
-  geom_boxplot() +
-  #facet_grid(~modelSpec) +
-  geom_hline(yintercept = .2) + 
-  geom_hline(yintercept = -.2) + 
-  geom_hline(yintercept = 0, color = "red", linetype = "dashed") +
-  scale_y_continuous(limits = c(-.5,.5), breaks = seq(from = -.8, to = .8, by = .2)) +
-  theme(axis.text.x=element_text(angle=45,hjust=1,vjust=0.5)) +
-  ggtitle("between: Missing Correlated Error")
-ggsave("finalResults/between Missing Correlated Error.jpeg")
+# convert all to character
+dm[, ] <- lapply(dm[, ], as.character)
+# give this function the subset you want and have it process the df
+processDF <- function(df) {
+  # make it long
+  df <- gather(df, parameter, est, l1.by.y1:lb.by.y6.sarg.p, factor_key = FALSE)
+  df$est <- as.numeric(df$est)
+  # put in true values
+  df[df$parameter=="l1.by.y1", "true"] <- 1
+  df[df$parameter=="l1.by.y2", "true"] <- .8
+  df[df$parameter=="l1.by.y3", "true"] <- .7
+  df[df$parameter=="l2.by.y4", "true"] <- 1
+  df[df$parameter=="l2.by.y5", "true"] <- .8
+  df[df$parameter=="l2.by.y6", "true"] <- .7
+  df[df$parameter=="l1.by.y5", "true"] <- .3
+  df[df$parameter=="l2.by.y2", "true"] <- .3
+  # tru values for between parameters
+  df[df$parameter=="lb.by.y2", "true"] <- .7
+  df[df$parameter=="lb.by.y3", "true"] <- .6
+  df[df$parameter=="lb.by.y4", "true"] <- .8
+  df[df$parameter=="lb.by.y5", "true"] <- .7
+  df[df$parameter=="lb.by.y6", "true"] <- .8
+  # Remove the scaling indicators
+  df <- df[which(df$parameter!="l1.by.y1"),]
+  df <- df[which(df$parameter!="l2.by.y4"),]
+  df <- df[which(df$parameter!="lb.by.y1"),]
+  # compute relative bias 
+  df$p_relBias <- ((df$est - df$true) / df$true)*100
+  # make a cluster var
+  df$cluster <- as.factor(paste0(df$clusterN,"groups_of_N", df$clusterSize))
+  # getting the order right
+  try(df$cluster <- relevel(df$cluster, " 30groups_of_N 30"))
+  try(df$cluster <- relevel(df$cluster, " 30groups_of_N100"))
+  try(df$cluster <- relevel(df$cluster, "100groups_of_N 30"))
+  try(df$cluster <- relevel(df$cluster, "100groups_of_N100"))
+  return(df)
+}
 
 
 
+# true model ------------------------------------------------------------------
+# subset what we want
+l_sub <- subset(dm, dm$skewKurt=="0000")
+l_sub <- subset(l_sub, l_sub$clusterBal=="bal")
+l_sub <- subset(l_sub, l_sub$modelSpec=="trueModel")
+# process the DF
+l_sub <- processDF(l_sub)
+# save it so .RMD can read it in
+saveRDS(l_sub, "./finalResults/trueModelComparisons.RDS")
+rm(list="l_sub")
+# -----------------------------------------------------------------------------
 
+# misspecW1  ------------------------------------------------------------------
+# subset what we want
+l_sub <- subset(dm, dm$skewKurt=="0000")
+l_sub <- subset(l_sub, l_sub$clusterBal=="bal")
+l_sub <- subset(l_sub, l_sub$modelSpec=="misSpecW1")
+# process the DF
+l_sub <- processDF(l_sub)
+# save it so .RMD can read it in
+saveRDS(l_sub, "./finalResults/MisW1Comparisons.RDS")
+rm(list="l_sub")
+# -----------------------------------------------------------------------------
+
+# misspecW2  ------------------------------------------------------------------
+# subset what we want
+l_sub <- subset(dm, dm$skewKurt=="0000")
+l_sub <- subset(l_sub, l_sub$clusterBal=="bal")
+l_sub <- subset(l_sub, l_sub$modelSpec=="misSpecW2")
+# process the DF
+l_sub <- processDF(l_sub)
+# save it so .RMD can read it in
+saveRDS(l_sub, "./finalResults/MisW2Comparisons.RDS")
+rm(list="l_sub")
+# -----------------------------------------------------------------------------
+# misspecW3  ------------------------------------------------------------------
+# subset what we want
+l_sub <- subset(dm, dm$skewKurt=="0000")
+l_sub <- subset(l_sub, l_sub$clusterBal=="bal")
+l_sub <- subset(l_sub, l_sub$modelSpec=="misSpecW3")
+# process the DF
+l_sub <- processDF(l_sub)
+# save it so .RMD can read it in
+saveRDS(l_sub, "./finalResults/MisW3Comparisons.RDS")
+rm(list="l_sub")
+# -----------------------------------------------------------------------------
+
+
+# skewKurtosis  ------------------------------------------------------------------
+# subset what we want
+# l_sub <- subset(dm, dm$skewKurt=="0000")
+l_sub <- subset(dm, dm$clusterBal=="bal")
+l_sub <- subset(l_sub, l_sub$modelSpec=="trueModel")
+l_sub <- subset(l_sub, l_sub$clusterN=="100")
+l_sub <- subset(l_sub, l_sub$clusterSize==" 30")
+# process the DF
+l_sub <- processDF(l_sub)
+# save it so .RMD can read it in
+saveRDS(l_sub, "./finalResults/skewKurt.RDS")
+rm(list="l_sub")
+# -----------------------------------------------------------------------------
+# 
+# balance  ------------------------------------------------------------------
+# subset what we want
+l_sub <- subset(dm, dm$skewKurt=="0000")
+l_sub <- subset(l_sub, l_sub$clusterBal=="unbal")
+l_sub <- subset(l_sub, l_sub$modelSpec=="trueModel")
+# l_sub <- subset(l_sub, l_sub$clusterN=="100")
+# l_sub <- subset(l_sub, l_sub$clusterSize==" 30")
+# process the DF
+l_sub <- processDF(l_sub)
+# save it so .RMD can read it in
+saveRDS(l_sub, "./finalResults/unbalance.RDS")
+rm(list="l_sub")
+# -----------------------------------------------------------------------------
