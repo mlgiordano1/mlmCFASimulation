f6ccd32d0cb2d64ec249a3e9483aa5ee4a616842
Michael Giordano <mlgiordano1@gmail.com>
2018-10-30 21:05:00 -0400

    changed the cluster naming in miiv function

--- SimulationFunctions.R
+++ SimulationFunctions.R
@@ -149,8 +149,6 @@ ANALYSIS:
 } # end makeData function
 
 
-
-
 # credit script to Francis L. Huang
 # Edited for my purposes
 # working paper on mcfa in R with Lavaan
@@ -165,18 +163,20 @@ mcfa.input<-function(l1Var, l2Var, dat){
     scaling <- (n^2-sum(freq$Freq^2)) / (n*(G-1))
     varn    <- names(dat2)
     ms      <- matrix(0,n,k)
-   for (i in 1:k){
+    # it appears as if the 'tibble' format might be causing issues
+   dat2 <- as.data.frame(dat2)
+    for (i in 1:k){
       ms[,i]<-ave(dat2[,i],g)
    }   
-   cs<-dat2-ms #deviation matrix, centered scores
-   colnames(ms)<-colnames(cs)<-varn
-   b.cov<-(cov(ms) * (n - 1))/(G-1) #group level cov matrix
-   w.cov<-(cov(cs) * (n - 1))/(n-G) #individual level cov matrix
-   pb.cov<-(b.cov-w.cov)/scaling #estimate of pure/adjusted between cov matrix
-   w.cor<-cov2cor(w.cov) #individual level cor matrix
-   b.cor<-cov2cor(b.cov) #group level cor matrix
-   pb.cor<-cov2cor(pb.cov) #estimate of pure between cor matrix
-   icc<-round(diag(pb.cov)/(diag(w.cov)+diag(pb.cov)),3) #iccs
+   cs           <- dat2-ms #deviation matrix, centered scores
+   colnames(ms) <- colnames(cs)<-varn
+   b.cov        <- (cov(ms) * (n - 1))/(G-1) #group level cov matrix
+   w.cov        <- (cov(cs) * (n - 1))/(n-G) #individual level cov matrix
+   pb.cov       <- (b.cov-w.cov)/scaling #estimate of pure/adjusted between cov matrix
+   w.cor        <- cov2cor(w.cov) #individual level cor matrix
+   b.cor        <- cov2cor(b.cov) #group level cor matrix
+   pb.cor       <- cov2cor(pb.cov) #estimate of pure between cor matrix
+   icc          <- round(diag(pb.cov)/(diag(w.cov)+diag(pb.cov)),3) #iccs
    return(list(b.cov=b.cov,pw.cov=w.cov,ab.cov=pb.cov,pw.cor=w.cor,
          b.cor=b.cor,ab.cor=pb.cor,
          n=n,G=G,c.=scaling,sqc=sqrt(scaling),
@@ -244,8 +244,8 @@ mlcfaMIIV <- function(withinModel,
   b <- MIIVsem::miive(betweenModel,
                       sample.cov = covMats[["between"]],
                       sample.nobs = g,
-                      overid.degree = 1, 
-                      overid.method = "random",
+                      # overid.degree = 1, 
+                      # overid.method = "random",
                       var.cov = var.cov)
   # return the list of within and between models
   return(list(within=w, between=b))
@@ -330,8 +330,6 @@ getVCE <- function(twoLvlnlmeObj, names) {
 }
 
 
-
-
 decompMuthen <- function(allIndicators,
                             l1Var,
                             l2Var,
@@ -355,7 +353,7 @@ decompMuthen <- function(allIndicators,
   # dropping the repeat "cluster" var
   agg <- agg[,-(ncol(agg)-1)]
   # adding names
-  names(agg) <- c("cluster", grpMnNames, "grpSize")
+  names(agg) <- c(l2Var, grpMnNames, "grpSize")
   
   #-------------------------------------------------------------------
   # within covariance matrices
@@ -376,7 +374,7 @@ decompMuthen <- function(allIndicators,
   for (i in seq(allIndicators)) {
     agg <- cbind(agg, mean(df[[allIndicators[i]]]))
   }
-  names(agg) <- c("cluster", grpMnNames, "grpSize", mnNames)
+  names(agg) <- c(l2Var, grpMnNames, "grpSize", mnNames)
   # difference matrix
   d <- as.matrix(agg[mnNames]-agg[grpMnNames])
   # covariance matrix
